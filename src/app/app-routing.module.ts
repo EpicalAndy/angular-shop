cbe50d76659d34e90079929244cf79b46b4d815e
SotskovA <sotskov.aa@mail.ru>
2022-03-23 23:31:46 +0300

    Homework 4.6 Added not-found module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,20 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 import { CatalogComponent } from './catalog/catalog.component';
+import { NoteFoundComponent } from "./note-found/note-found.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: CatalogComponent
+  },
+  {
+    path: 'not-found',
+    component: NoteFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
   }
 ]
 
