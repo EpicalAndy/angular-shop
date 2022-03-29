b6d472015e9dbff6627ca693cd64f267d771b194
SotskovA <sotskov.aa@mail.ru>
2022-03-29 13:11:50 +0300

    Homework 4.6 1 Update

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+
+import { products } from "../products.data";
 
 @Component({
   selector: 'app-product-page',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent implements OnInit {
+  public productId = ''
+  public productData: any
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.productId = this.route.snapshot.params['id'];
+    this.productData = products.find((product) => {return `${product['id']}` === this.productId});
   }
 
+  ngOnInit(): void {
+    this.isNoHaveProduct() && this.goTo404Page();
+  }
+
+  private isNoHaveProduct() {
+    return !this.productData;
+  }
+
+  private goTo404Page() {
+    this.router.navigate(['/404']);
+  }
 }
--- src/app/product-page/product-page.module.ts
+++ src/app/product-page/product-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductPageComponent } from './product-page.component';
 
+import { CardModule } from "../card/card.module";
 import { ProductPageRoutingModule } from "./product-page-routing.module";
 
 
@@ -11,7 +12,8 @@ import { ProductPageRoutingModule } from "./product-page-routing.module";
   ],
   imports: [
     CommonModule,
-    ProductPageRoutingModule
+    ProductPageRoutingModule,
+    CardModule
   ]
 })
 export class ProductPageModule { }
